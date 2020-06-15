d0a1ee2a2461046706ab0a954ab6a39e3488ceb4
Tamar  Regev <tamaregev@gmail.com>
2020-06-15 12:42:36 -0400

    modify f0 cleaning+ plot process

--- plotStimF0.m
+++ plotStimF0.m
@@ -1,22 +1,38 @@
-function [h,f0_in,t0_in] = plotStimF0(y,fs,params)
+function [h,f0_in,t0_in] = plotStimF0(y,fs,params, showProcess)
 % plotStimF0 Calculates fundamental frequency using STRAIGHT, cleans it and plots signal+f0+spectrogram
 % 
 % Uses: STRAIGHT (Hideki Kawahara)
 %       myspectrogram (by Kamil Wojcicki, Mathworks file exchange) - https://www.mathworks.com/matlabcentral/fileexchange/29596-speech-spectrogram
 %
-% Inputs: params is a structure
+% Inputs:  y and fs are the outpts of [y,fs] = audioread('<soundFile>');
+%
+%          params is a structure
 %          Example:
-%               params.th_f0power = 0.017;threshold f0 power for plotting f0
-%               params.th_df = 10;%Hz maximal f0 jump for plotting f0
+%               params.th_f0score = 0.75;threshold f0 score for plotting f0 (score is 0 to 1)
+%               params.th_df = 95;%Percentile maximal f0 jump for plotting f0
 %               params.conv = 5;
+%
+%           showProcess is an optional logical varible. If showProcess = true a graph showing the f0 cleaning process will be displayed. Default: true. 
+
 
 %
-% Tamar Regev, June 2020
+% June 2020, Tamar Regev, initial version
+% June 15 2020, Tamar Regev
+%                - Changed f0 cleaning process.
+%                    input params subfield params.th_f0power replaced by params.th_f0score 
+%                - Added plot to show process of f0 cleaning + optional
+%                input variable showProcess
+% 
 %% Definitions
-STRAIGHTFolder = 'GitHub/plot_fundamental/STRAIGHT/TandemSTRAIGHTmonolithicPackage010';
+function_dir = '/Users/tamaregev/Dropbox/postdoc/Fedorenko/Prosody/Prosody-meaning/GitHub/plot_fundamental';
+STRAIGHTFolder = [function_dir '/STRAIGHT/TandemSTRAIGHTmonolithicPackage010'];
 addpath(STRAIGHTFolder)
-addpath(['GitHub/plot_fundamental/myspectrogram'])
+addpath([function_dir '/myspectrogram'])
 
+if ~exist('showProcess','var')
+    showProcess=true;
+end
+    
 %% Analysis
 t = (0:length(y)-1)/fs;
 
@@ -24,79 +40,82 @@ r = exF0candidatesTSTRAIGHTGB(y,fs); % Extract F0 information
 rc = autoF0Tracking(r,y); % Clean F0 trajectory by tracking
 rc.vuv = refineVoicingDecision(y,rc);
 q = aperiodicityRatioSigmoid(y,rc,1,2,0); % aperiodicity extraction
-f = exSpectrumTSTRAIGHTGB(y,fs,q); 
+%f = exSpectrumTSTRAIGHTGB(y,fs,q); 
 
-    f0power = q.f0candidatesPowerMap(1,:)';
-    f0_in = rc.f0;
-    t0_in=rc.temporalPositions;
+%f0power = q.f0candidatesPowerMap(1,:)';
+f0score = q.f0CandidatesScoreMap(1,:)';
+f0_in = rc.f0;
+t0_in=rc.temporalPositions;
 
-    %select areas with high f0 power
-    th_f0power=params.th_f0power;
-    f0_in(f0power<th_f0power)=nan;
-    t0_in(f0power<th_f0power)=nan;
+%select areas with high f0 power   
+% th_f0power=max(q.f0candidatesPowerMap(1,:)')/params.th_f0power;
+%th_f0power=prctile(q.f0candidatesPowerMap(1,:),45);
+
+%select areas with high f0 score   
+th_f0score =  params.th_f0score;%typically 0.75
+
+
+%select areas with big jumps:
+th_df=prctile(diff(f0_in),params.th_df);
+df0=nan(size(f0_in));
+df0(1:end-1)=abs(diff(f0_in));
+
+if showProcess
+    figure;
+    subplot(2,1,1)
+    plot(rc.temporalPositions,f0score,'.');grid on;hold on
+    plot([rc.temporalPositions(1),rc.temporalPositions(end)],[th_f0score,th_f0score])
+    xlim([0 t(end)])
+    title('F0 score + threshold')
+    set(gca,'fontsize',14)
     
-    %clean f0:
-    %exclude big jumps:
-    th_df = params.th_df;%Hz
-    df0=nan(size(f0_in));
-    df0(1:end-1)=abs(diff(f0_in));
-    f0_in(df0>th_df)=nan;
-    t0_in(df0>th_df)=nan;
-    %exclude isolated points:
-%     
-    %smooth:
-    convolution = ones(1,params.conv)/params.conv;
-   f0_in=conv(f0_in,convolution,'same');
-   
+    subplot(2,1,2)
+    plot(t0_in,f0_in,'.','markersize',14);grid on;hold on
+    ylabel('f0 (Hz)');
+    ylim([50 350])
+    xlim([0 t(end)])
+    plot(t0_in(df0>th_df),f0_in(df0>th_df),'.r','markersize',18)
+    plot(t0_in(f0score<th_f0score),f0_in(f0score<th_f0score),'.g')
+    title('raw + cleaned F0')
+    legend('clean F0','rejected due to large df','rejected due to low score','fontsize',12,'location','sw')
+    set(gca,'fontsize',14)
+    
+end
+
+%smooth:
+convolution = ones(1,params.conv)/params.conv;
+f0_in=conv(f0_in,convolution,'same');
+
+%clean f0:
+f0_in(df0>th_df)=nan;
+t0_in(df0>th_df)=nan;
+f0_in(f0score<th_f0score)=nan;
+t0_in(f0score<th_f0score)=nan;
+    
 
 %% plot
-h=figure;
-     subplot(3,1,1); plot(t,y);xlim([0 t(end)]); grid on
-     set(gca,'fontsize',14)
-%      subplot(7,1,2); plot(t0,f0); grid on
-%         ylabel('Pitch (Hz)')
-%         ylim([50 250])
-%         xlim([0 t(end)])
+    h=figure;
+    
+    %%% plot signal
+    subplot(3,1,1); plot(t,y);xlim([0 t(end)]); grid on
+    set(gca,'fontsize',14)
         
-        subplot(3,1,2)
-        %%% plot fundamental 
-%         plot(rc.temporalPositions,rc.f0);grid on
-%         set(gca,'fontsize',14);
-%         xlabel('Time (s)')
-%         ylabel('fundamental frequency (Hz)');
-%         ylim([50 250])
-%         xlim([0 t(end)])
-
-        plot(t0_in,f0_in,'linewidth',2);grid on
-        
-        ylabel('f0 (Hz)');
-        ylim([50 250])
-        xlim([0 t(end)])
-        set(gca,'fontsize',14)
-%        subplot(7,1,4);hold off
-%         %figure
-%         plot(rc.temporalPositions,q.f0candidatesPowerMap(1,:)') 
-%         xlim([0 t(end)])
-%         grid on
-%         
-%         subplot(7,1,5);hold off
-%         plot(rc.temporalPositions,q.periodicityLevel) 
-%         xlim([0 t(end)])
-%         grid on
-
-        subplot(3,1,3)
-        [hs, T, F] = myspectrogram(y, fs, [22 1], @hamming, 2048, [-59 -1], false, 'default', false, 'per'); % or be quite specific about what you want
-        ylim([0 2000])
-        xlim([0 t(end)])
-        colormap('parula')
-        xlabel('Time (s)')
-        set(gca,'fontsize',14)
-%         subplot(7,1,7)
-%         ss=sum(hs.CData);
-%         plot(T,ss)
-%         xlim([T(1) T(end)])
-%         grid on
-
+    %%% plot fundamental 
+    subplot(3,1,2)
+    plot(t0_in,f0_in,'.');grid on
+    ylabel('f0 (Hz)');
+    ylim([50 350])
+    xlim([0 t(end)])
+    set(gca,'fontsize',14)
+    
+    %%% plot spectrogram
+    subplot(3,1,3)
+    [hs, T, F] = myspectrogram(y, fs, [22 1], @hamming, 2048, [-59 -1], false, 'default', false, 'per'); % or be quite specific about what you want
+    ylim([0 2000])
+    xlim([0 t(end)])
+    colormap('parula')
+    xlabel('Time (s)')
+    set(gca,'fontsize',14)
 
 end
 
