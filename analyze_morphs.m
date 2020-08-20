82128690206b95de15148a4040898dd3872e721e
agdawson <agdawson@mit.edu>
2020-08-19 22:42:32 -0400

    Update analyze_morphs.m
    fixed bugs

--- analyze_morphs.m
+++ analyze_morphs.m
@@ -6,9 +6,11 @@ params.my_cal_factor = 1.0;  %the value for your system to convert the WAV into 
 
 D = dir; % A is a struct ... first elements are '.' and '..' used for navigation.
 D = D(~ismember({D.name}, {'.', '..'})); % dir returns '.' and '..' (usually in first slot)
-                                                                % , filter them out for eval.
-varheaders = ["sent","condition","sum_pitch_diff","max_pitch_diff",...
-          "sum_loud_diff","max_loud_diff"]; % table headers
+                                            % , filter them out for eval.
+
+D = D(isfolder({D.name})); %only look at subdirectories
+% varheaders = ["sent","condition","sum_pitch_diff","max_pitch_diff",...
+          % "sum_loud_diff","max_loud_diff"]; % table headers
 counter = 1; % to keep track of total iterations and row assignment
 
 sent = strings(81,1);
@@ -20,7 +22,7 @@ maxloud = zeros(81,1);
 
 for ii = 1:numel(D) % iterate over every sound file
       currD = D(ii).name; % get the current subdirectory name
-      sent_name = extractAfter(D(ii).name,'sent');
+      sent_name = extractAfter(D(ii).name,'Sent');
       cd(currD) % enter the subdirectory
       soundfiles = dir('*.wav'); % get all wav files in current subdirectory
 % %% Identify which wav file is the neutral base
@@ -32,15 +34,24 @@ for ii = 1:numel(D) % iterate over every sound file
 %             baseelementnumber = jj;
 %         end
 %       end
-%% If we assume the second wav file will always be the base.
-      base_neutral = soundfiles(2);
-      [y1,fs] = audioread(base_neutral.name);
+%% Locate the neutral base
+%       base_neutral = soundfiles(2);
+%       [y1,fs] = audioread(base_neutral.name);
+      for pp = 1:4
+          current_soundname = extractBetween(soundfiles(pp).name,'_','.wav');
+          if current_soundname{1,1} == 'N'
+              base_neutral = soundfiles(pp);
+              [y1,fs] = audioread(base_neutral.name);
+              break % exit loop once the neutral base is found
+          end
+      end
+%% Analysis of Morphs 
       for kk = 1:4
-          if kk == 2 % if it is the second iteration, this is the neutral base, do not evaluate
+          if kk == pp % if it is the neutral base, do not evaluate
               continue
           else
               [y2,~] = audioread(soundfiles(kk).name);
-              [delta_pitch,delta_loud,~,~,~,~,~] = pitch_loud_diff(y1,y2,fs,params,false);
+              [delta_pitch,delta_loud, p1, p2, t_p, l1, l2, t_l] = pitch_loud_diff(y1,y2,fs,params,false);
               
               sent(counter,1) = sent_name;
               cellarray_cond = extractBetween(soundfiles(kk).name,'_','.wav');
@@ -53,6 +64,7 @@ for ii = 1:numel(D) % iterate over every sound file
               counter = counter + 1;
           end  
       end
+      disp(ii)
       cd('..') % return to main directory
 end
 T = table(sent,condition,sumpitch,maxpitch,sumloud,maxloud);
